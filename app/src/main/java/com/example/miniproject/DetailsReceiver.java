4caace8d71fb12391bc95faf838b7ce8f6d02858
weisheng <weisheng_rules@hotmail.com>
2021-06-08 16:24:29 +0800

    added phone and email intent

--- app/src/main/java/com/example/miniproject/DetailsReceiver.java
+++ app/src/main/java/com/example/miniproject/DetailsReceiver.java
@@ -3,6 +3,7 @@ package com.example.miniproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -71,6 +72,25 @@ public class DetailsReceiver extends AppCompatActivity {
                 startActivity(editReceiver);
             }
         });
+
+        textPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phonecall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+textPhone.getText().toString()));
+                startActivity(phonecall);
+            }
+        });
+
+        textEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SENDTO);
+                email.setData(Uri.parse("mailto:"+textEmail.getText().toString()));
+                startActivity(email);
+            }
+        });
+
+
 
 //        addBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
